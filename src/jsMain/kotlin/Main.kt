2fafed3901081e280f37af5665328974bada1291
Pushkar Anand <anandpushkar088@gmail.com>
2023-02-03 22:52:12 +0530

    :tada: Setup Project

---
+++ src/jsMain/kotlin/Main.kt
@@ -0,0 +1,16 @@
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import org.jetbrains.compose.web.css.*
+import org.jetbrains.compose.web.dom.*
+import org.jetbrains.compose.web.renderComposable
+
+fun main() {
+    renderComposable(rootElementId = "root") {
+        H1 {
+            Text("Hello, World!")
+        }
+    }
+}
+
+
